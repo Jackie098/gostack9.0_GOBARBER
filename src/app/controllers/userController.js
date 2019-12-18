9e56aa239c4c6dd2c3fa15808688a5f0b1587278
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-17 22:41:16 -0300

    create: Rota e método no controller para adicionar usuário

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,10 +1,21 @@
 import User from '../models/User';
 
 class UserController {
+  // eslint-disable-next-line class-methods-use-this
   async store(req, res) {
-    const user = await User.create(req.body);
+    const userExists = await User.findOne({ where: { email: req.body.email } });
 
-    return res.json(user);
+    if (userExists)
+      return res.status(400).json({ error: 'User already exists.' });
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,7 @@
 import { Router } from 'express';
-import User from './app/models/User';
 import UserController from './app/controllers/userController';
 
 const routes = new Router();
-
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '12132133',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
