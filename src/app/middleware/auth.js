69bd4453cce9c374a23a773b3326af38ad704afb
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-19 20:03:13 -0300

    Implementando a função de alterar usuário

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -18,11 +18,11 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    console.log(decoded);
+    req.userId = decoded.id;
+
+    return next();
   } catch (err) {
     // Se retornar erro é pq o token é inválido
     res.status(401).json({ error: 'Token Invalid' });
   }
-
-  return next();
 };
