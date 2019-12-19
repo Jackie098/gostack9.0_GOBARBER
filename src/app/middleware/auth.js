e0f22e7666fd3a5192da137a88e2b4b1eec4429c
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-19 17:36:14 -0300

    fix: Middleware de autenticação, processo de tokenização de usuário

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable consistent-return */
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util'; // Pega uma função callback(ultrapassado) e me permite utilizar nela async/await
 
@@ -12,19 +14,11 @@ export default async (req, res, next) => {
 
   const [, token] = authHeader.split(' '); // Este método divide a string (bearer -token-) por cada espaço e aloca em um vetor
   // Com a desestruturação eu estou utilizando apenas o segundo valor do vetor que é exatamente o token, tendo em vista que o primeiro valor é o 'bearer'
-  console.log(`Parâmetro-> ${token}\nClasse-> ${authConfig.secret} `);
-  try {
-    let ui;
-    // const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
-      if (err) return res.status(500).json(err);
-      ui = decoded;
-      console.log(ui);
-      next();
-    });
-    // eslint-disable-next-line no-console
 
-    // return next();
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    console.log(decoded);
   } catch (err) {
     // Se retornar erro é pq o token é inválido
     res.status(401).json({ error: 'Token Invalid' });
