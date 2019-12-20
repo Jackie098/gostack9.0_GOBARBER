b94621f7852b301f5ed998cf838117ca42996346
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-20 01:03:36 -0300

    Validação de dados de entrada com yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,26 @@
+import * as Yup from 'yup'; // Neste módulo não tem 'export defaul e por isso sua importação é feita de maneira diferete
+
 import User from '../models/User';
 
 class UserController {
   // eslint-disable-next-line class-methods-use-this
   async store(req, res) {
+    // Yup define a forma de como eu quero o corpo da requisição
+    const schema = Yup.object().shape({
+      // 'required' são os campos obrigatórios, 'email' ele valida tudo certinho, e 'min' é a quantidade mínima de caracteres permitidos
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists)
@@ -19,6 +37,27 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ), // 'password' só é utilizada se oldPassword estiver preechido
+      confirmPassword: Yup.string().when(
+        'password',
+        (password, field) =>
+          password ? field.required().oneOf([Yup.ref('password')]) : field
+        // Se 'password' estiver preechido, este campo se torna obrigatorio e o valor dele tem que ser igual ao do 'password'
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
