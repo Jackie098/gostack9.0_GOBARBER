f21ae838f3c8fbb07d3c7d2b060ccfc97ce51fda
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-20 01:38:23 -0300

    fix: Bug na validação dos dados na sessão

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,7 +7,7 @@ import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    const schema = Yup.object.shape({
+    const schema = Yup.object().shape({
       email: Yup.string()
         .email()
         .required(),
